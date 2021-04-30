6d31b25cce5eb9fbee3951732919d456638f8f1c
achaves100 <adan.chaves@outlook.com>
2021-04-30 14:11:59 +0000

    Actualizando Ruteo

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -21,21 +21,6 @@ export const Navbar = () => {
 				<Link to="/test">
 					<button className="btn btn-primary">test</button>
 				</Link>
-				<Link to="/cuentacrear">
-					<button className="btn btn-success">Crear Cuenta</button>
-				</Link>
-				<Link to="/cuentainiciar">
-					<button className="btn btn-success">Inicio Sesión</button>
-				</Link>
-				<Link to="/cuentarecuperar1">
-					<button className="btn btn-warning">Recuperar Cuenta 1</button>
-				</Link>
-				<Link to="/cuentarecuperar2">
-					<button className="btn btn-warning">Recuperar Cuenta 2</button>
-				</Link>
-				<Link to="/cuentarecuperar3">
-					<button className="btn btn-warning">Recuperar Cuenta 3</button>
-				</Link>
 			</div>
 		</nav>
 	);
