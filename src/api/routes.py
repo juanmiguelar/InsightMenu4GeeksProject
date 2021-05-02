c723a3009efc7610847a138e7c217725b33d2618
Juan Miguel <juanmiguelar09@gmail.com>
2021-05-02 00:47:12 +0000

    Primeras rutas

--- src/api/routes.py
+++ src/api/routes.py
@@ -2,17 +2,27 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Pedido, DetallePedido, Producto, Ingrediente, Producto_x_Tag, Tag
 from api.utils import generate_sitemap, APIException
 
 api = Blueprint('api', __name__)
 
-
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
-
+@api.route('/alive', methods=['GET'])
+def alive():
     response_body = {
-        "message": "Hello! I'm a message that came from the backend"
+        "message": "Alive"
     }
-
     return jsonify(response_body), 200
+
+@api.route('/producto', methods=['GET'])
+def GetAllProduct():
+    productos = list(map(lambda p: p.serialize(), Producto.query.all()))
+    return jsonify(productos), 200     
+
+@api.route('/producto/<int:id>', methods=['GET'])
+def GetProductById(id):
+    producto = Producto.query.get(id)
+    if producto is not None:
+        return jsonify(producto.serialize()), 200     
+    else:
+        return "El producto no existe", 404
