e06800eec8966cc7b22c0a9b9265790275801139
Juan Miguel <juanmiguelar09@gmail.com>
2021-05-08 02:28:29 +0000

    login

--- src/api/routes.py
+++ src/api/routes.py
@@ -11,12 +11,8 @@ from flask_jwt_extended import get_jwt_identity
 from flask_jwt_extended import jwt_required
 from flask_jwt_extended import JWTManager
 
-
 api = Blueprint('api', __name__)
 
-# Setup the Flask-JWT-Extended extension
-# api.config["JWT_SECRET_KEY"] = "super-secret"  # Change this!
-# jwt = JWTManager(api)
 
 @api.route('/alive', methods=['GET'])
 def alive():
