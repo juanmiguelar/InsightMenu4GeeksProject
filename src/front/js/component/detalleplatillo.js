778f73c06bb88e864659dc606bb7eec34288ff8a
Margoth <mloreana@gmail.com>
2021-05-14 02:42:08 +0000

    Agregar carrito mejorado

--- src/front/js/component/detalleplatillo.js
+++ src/front/js/component/detalleplatillo.js
@@ -198,7 +198,7 @@ export const Detalleplatillo = () => {
 	return (
 		<div className="container">
 			<div className="row">
-				<div className="col-xs-12 col-sm-3 col-md-4 col-lg-5">
+				<div className="col-xs-12 col-sm-3 col-md-4 col-lg-5 mb-2">
 					<div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
 						<ol className="carousel-indicators">
 							<li data-target="#carouselExampleIndicators" data-slide-to="0" className="active" />
